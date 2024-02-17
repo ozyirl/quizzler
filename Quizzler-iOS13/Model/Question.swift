2500a7080b19c4f1de55e866dfa7dcc0620364ca
ozy <ozyftw17@gmail.com>
2024-02-18 02:19:09 +0530

    day 2

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by ozy  on 17/02/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
---
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,9 @@
+//
+//  QuizBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by ozy  on 18/02/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
