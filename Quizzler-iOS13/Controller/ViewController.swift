d336ca1cc33dfb717e529cb02c53cc9e4e8e5347
ozy <ozyftw17@gmail.com>
2024-02-19 00:12:03 +0530

    mcq added

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -9,69 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var falseBtn: UIButton!
-    @IBOutlet weak var trueBtn: UIButton!
+  
+    @IBOutlet weak var choice2: UIButton!
+    
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice3: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let quiz = [
-        Question(q: "A slug's blood is green.", a: "True"),
-              Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-              Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-              Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-              Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-              Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-              Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-              Question(q: "Google was originally called 'Backrub'.", a: "True"),
-              Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-              Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-              Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-              Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
 
-        
-       
-    ]
-  
-    var questionNumber = 0
+  var quizBrain = QuizBrain()
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateUi()
+        updateUI()
         
     }
 
     @IBAction func answerBtnPressed(_ sender: UIButton) {
-        let userAnswer = sender.currentTitle
-        let actualAnswer = quiz[questionNumber].answer
-        if userAnswer == actualAnswer {
-            sender.backgroundColor = UIColor.green
-        } else{
-            sender.backgroundColor = UIColor.red
-        }
+        let userAnswer = sender.currentTitle!
+               
+               let userGotItRight = quizBrain.checkAnswer(userAnswer: userAnswer)
+               
+               if userGotItRight {
+                   sender.backgroundColor = UIColor.green
+               } else {
+                   sender.backgroundColor = UIColor.red
+               }
         
-        if questionNumber + 1 < quiz.count {
-           
-            questionNumber += 1
-            
-            
-            
-        } else {
-            questionNumber = 0
-        }
+        quizBrain.nextQuestion()
         
-        Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateUi), userInfo:nil, repeats: false)
+        Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateUI), userInfo:nil, repeats: false)
        
         
     }
     
-  @objc func updateUi(){
+    @objc func updateUI() {
+         questionLabel.text = quizBrain.getQuestionText()
+         
+         //Need to fetch the answers and update the button titles using the setTitle method.
+         let answerChoices = quizBrain.getAnswers()
+         choice1.setTitle(answerChoices[0], for: .normal)
+         choice2.setTitle(answerChoices[1], for: .normal)
+         choice3.setTitle(answerChoices[2], for: .normal)
+         
+         progressBar.progress = quizBrain.getProgress()
+         scoreLabel.text = "Score: \(quizBrain.getScore())"
+         
+         choice1.backgroundColor = UIColor.clear
+         choice2.backgroundColor = UIColor.clear
+         
+         //Third button needs to be reset too.
+         choice3.backgroundColor = UIColor.clear
         
-        questionLabel.text = quiz[questionNumber].text
-        trueBtn.backgroundColor = UIColor.clear
-        falseBtn.backgroundColor = UIColor.clear
-      progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
-        
-    }
+        if quizBrain.questionNumber == 0 {
+            quizBrain.score = 0 
+        }
+         
+     }
+
+ 
     
 }
 
--- Quizzler-iOS13/Model/Question.swift
+++ Quizzler-iOS13/Model/Question.swift
@@ -8,13 +8,17 @@
 
 import Foundation
 
-
 struct Question {
     let text: String
-    let answer: String
     
-    init(q: String, a: String) {
+    
+    let answers: [String]
+
+    let rightAnswer: String
+
+    init(q: String, a: [String], correctAnswer: String) {
         text = q
-        answer = a
+        answers = a
+        rightAnswer = correctAnswer
     }
 }
